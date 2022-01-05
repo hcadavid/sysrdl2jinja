c198859bbc5b51c0c1faf3b2e3868d450ce0c002
Hector Cadavid <hector.cadavid@gmail.com>
2022-01-05 17:56:18 +0100

    setup script

--- setup.sh
+++ setup.sh
@@ -1,5 +1,5 @@
 python -m venv .
 source bin/activate
 pip install -r requirements.txt
-source bin/deactivate
+deactivate
 chmod +x sysrdl2jinja.sh
--- sysrdl2jinja.sh
+++ sysrdl2jinja.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 source bin/activate
 python3 systemrdl2jinjatemplate.py $1 $2 $3
-source bin/deactivate
+deactivate
