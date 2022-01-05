64cfc2aef762f8d71047ce4425ce235efa7df701
Hector Cadavid <hector.cadavid@gmail.com>
2022-01-05 12:55:56 +0100

    parsing part

--- systemrdl2jinjatemplate.py
+++ systemrdl2jinjatemplate.py
@@ -1,56 +1,17 @@
 import sys
 import os
-from systemrdl.parser.sa_systemrdl import SA_ErrorListener
-from systemrdl import preprocessor
-from systemrdl.parser import sa_systemrdl
-import print_tree
-import sys
-import base64
-import zlib
-import print_tree
-from print_tree import OtherTraverseListener
-from systemrdl import RDLListener
-from systemrdl.node import FieldNode, Node
-from systemrdl import RDLCompiler, RDLCompileError, RDLWalker
-from systemrdl.parser import sa_systemrdl
-from systemrdl import preprocessor
-from systemrdl.parser.sa_systemrdl import SA_ErrorListener
-from antlr4 import InputStream, Token
 
 
-class __DetailedSyntaxErrorListener(SA_ErrorListener):
-
-    def __init__(self):
-        self.syntax_error_details = None
-        self.count = 0
-
-    def syntaxError(self, input_stream: InputStream, offending_symbol: Token, char_index: int, line: int, column: int,
-                    msg: str):
-        self.syntax_error_details = RDLSyntaxError(offending_symbol, char_index, line, column, msg)
-
-
-def __get_detailed_syntax_error(rdl_file_path):
-    rdlc2 = RDLCompiler()
-    input_stream = preprocessor.preprocess_file(rdlc2.env, rdl_file_path, [])
-    se_listener = __DetailedSyntaxErrorListener()
-    parsed_tree = sa_systemrdl.parse(
-        input_stream,
-        "root",
-        se_listener
-    )
-    return se_listener.syntax_error_details
-
-
-def parse_rdl_file(rdl_file):
+def parse_rdl_file(_rdl_file):
     # Create an instance of the compiler
     rdlc = RDLCompiler()
 
-    rdlc.compile_file(rdl_file)
+    rdlc.compile_file(_rdl_file)
 
     # Elaborate the design
     root = rdlc.elaborate()
 
-    registers = []
+    _registers = []
 
     # iterate over registries definition
     for register_def in root.top.inst.children:
@@ -76,8 +37,8 @@ def parse_rdl_file(rdl_file):
                 field["reset"] = "undefined"
             fields.append(field)
 
-    registers.append(register)
-    return registers
+    _registers.append(register)
+    return _registers
 
 
 if __name__ == "__main__":
@@ -91,8 +52,9 @@ if __name__ == "__main__":
     try:
         registers = parse_rdl_file(rdl_file)
         print(registers)
+        exit(0)
     except RDLCompileError as exc:
-        # the document is parsed again with a custom listener to get the details of the syntax error
-        # this is done due to a limitation of the RDLCompiler API
-        detailed_se = __get_detailed_syntax_error(rdl_file)
-        print(detailed_se)
+        # error details are sent to stderr by the parser, next line add further details
+        print("File: ", rdl_file, file=sys.stderr)
+        exit(1)
+        
