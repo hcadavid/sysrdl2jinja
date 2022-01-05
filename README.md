76cef71413927d056dd2188e72238afebedb1872
Hector Cadavid <hector.cadavid@gmail.com>
2022-01-05 15:11:30 +0100

    folder structure organized

---
+++ README.md
@@ -0,0 +1,1 @@
+python systemrdl2jinjatemplate.py rdlsamples/rover.rdl templates/sysrdl_adoc_table.template
--- systemrdl2jinjatemplate.py
+++ systemrdl2jinjatemplate.py
@@ -24,11 +24,16 @@ def parse_rdl_file(_rdl_file):
             field = dict()
             field["id"] = field_def.inst_name
             field["low"] = field_def.low
+            field["high"] = field_def.high
             field["lsb"] = field_def.lsb
             field["msb"] = field_def.msb
             field["width"] = field_def.width
+            if "name" in field_def.properties:
+                field["name"] = field_def.properties["name"]
+            else:
+                field["name"] = "undefined"
             if "hw" in field_def.properties:
-                field["rights"] = field_def.properties["hw"].value
+                field["rights"] = field_def.properties["hw"]
             else:
                 field["rights"] = "undefined"
             if "reset" in field_def.properties:
@@ -37,24 +42,32 @@ def parse_rdl_file(_rdl_file):
                 field["reset"] = "undefined"
             fields.append(field)
 
-    _registers.append(register)
+        _registers.append(register)
+
     return _registers
 
 
 if __name__ == "__main__":
     from systemrdl import RDLCompiler, RDLCompileError, RDLWalker
-    from RDLModelListeners import MyModelPrintingListener
     from jinja2 import Template
+
+    if len(sys.argv) < 3:
+        print("SystemRDL to Jinja template converter", file=sys.stderr)
+        print("Syntax: ", sys.argv[0], " [rdl_file] [jinja_template_file]", file=sys.stderr)
+        exit(1)
+
 
     # Collect input files from the command line arguments
     rdl_file = sys.argv[1]
+    template_file = sys.argv[2]
 
     try:
         registers = parse_rdl_file(rdl_file)
-        print(registers)
+        with open(template_file) as file_:
+            template = Template(file_.read())
+        print(template.render(registers=registers))
         exit(0)
     except RDLCompileError as exc:
         # error details are sent to stderr by the parser, next line add further details
         print("File: ", rdl_file, file=sys.stderr)
         exit(1)
-        
