081dbc777f56d00e0c90533f1ca71d6b1e3643f1
Hector Cadavid <hector.cadavid@gmail.com>
2022-01-12 15:21:33 +0100

    script updated

--- convert_to_adoc.sh
+++ convert_to_adoc.sh
@@ -3,9 +3,11 @@ PYTHON_ENV_INTERPRETER=bin/python3
 
 if test -f "$PYTHON_ENV_INTERPRETER"; then
 	echo "Python local environment with dependencies installed"
+	source bin/activate
 else
 	echo "Setting up local environment and dependencies"
 	python3 -m venv .
+	source bin/activate
 	pip install -r requirements.txt
 fi
 
