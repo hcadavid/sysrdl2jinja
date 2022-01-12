cab00637f8933d1f1315eff766d51160d5bbc2d5
Hector Cadavid <hector.cadavid@gmail.com>
2022-01-12 16:27:14 +0100

    libraries broken dependency fixed

--- convert_to_adoc.sh
+++ convert_to_adoc.sh
@@ -1,15 +1,5 @@
+#This script assumes the python dependencies defined in dependencies.txt are already available
 cd $(dirname $0)
-PYTHON_ENV_INTERPRETER=bin/python3
-
-if test -f "$PYTHON_ENV_INTERPRETER"; then
-	echo "Python local environment with dependencies installed"
-	source bin/activate
-else
-	echo "Setting up local environment and dependencies"
-	python3 -m venv .
-	source bin/activate
-	pip install -r requirements.txt
-fi
 
 python3 sysrdl2jinja/convert.py $1 templates/sysrdl_adoc_table.template $2
 
